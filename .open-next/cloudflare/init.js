0a93e2bb0ab00d62e1d6aeed8e16fd062b6761b6
Jeevan Shah <jedimaster3284@gmail.com>
2025-10-23 18:06:34 -0400

    prepare for deployment

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1761256187309,
+    __BUILD_TIMESTAMP_MS__: 1761257123973,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
